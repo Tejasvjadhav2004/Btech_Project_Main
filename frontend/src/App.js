c79088aae29e9d910d48f07de76ebf3d1a38a26e
Tejas <tejasvjadhav2004@gmail.com>
2026-03-03 23:21:32 +0530

    starter code added

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the BTech Project Main1 Frontend</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
